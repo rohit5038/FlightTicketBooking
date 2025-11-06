3bb727b62a375418be9f85896e8b30b38d641691
Rohit <rjjaiswal224.com>
2025-11-06 21:07:59 +0530

    Cleaned repo - ignored cache and migration files

--- capstone/settings.py
+++ capstone/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-                    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'flight_ticket_booking',
+        'USER':'postgres',
+        'HOST':'localhost',
+        'PASSWORD':'root',
+        'PORT':'5432',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
